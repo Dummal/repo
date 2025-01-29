4cf54d861c2b4976d0362d501894953d7ee6bb2b
Dummal <soundari1129@gmail.com>
2025-01-29 16:36:19 +0530

    Add Terraform configuration files

--- main.tf
+++ main.tf
@@ -8,29 +8,22 @@ resource "aws_s3_bucket" "aft_logs_bucket" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm     = "aws:kms"
+        sse_algorithm   = "aws:kms"
         kms_master_key_id = aws_kms_key.aft_kms_key.arn
       }
     }
   }
 
-  tags = {
-    Environment = "Production"
-    ManagedBy   = "Terraform"
-  }
-
-  lifecycle_rule {
-    enabled = true
-    noncurrent_version_expiration {
-      days = 30
-    }
-  }
-
-  public_access_block {
+  block_public_access {
     block_public_acls       = true
     block_public_policy     = true
     ignore_public_acls      = true
     restrict_public_buckets = true
+  }
+
+  tags = {
+    Environment = "Production"
+    ManagedBy   = "Terraform"
   }
 }
 
@@ -41,6 +34,7 @@ resource "aws_kms_key" "aft_kms_key" {
 
   policy = jsonencode({
     Version = "2012-10-17"
+    Id      = "key-default-1"
     Statement = [
       {
         Sid       = "Enable IAM User Permissions"
@@ -64,7 +58,7 @@ resource "aws_kms_key" "aft_kms_key" {
           "kms:GenerateDataKey*",
           "kms:DescribeKey"
         ]
-        Resource = "*"
+        Resource  = "*"
       }
     ]
   })
